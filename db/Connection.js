23fa29c1c2835a4fb836c2b65e14f513e422495b
Matt Luna <mat.c.luna@gmail.com>
2021-10-29 18:57:14 -0500

    stuff

--- db/Connection.js
+++ db/Connection.js
@@ -1,13 +1,75 @@
 const mysql = require("mysql2");
+require("dotenv").config();
+const util = require("util");
+
 // Connect to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // Your MySQL username,
-    user: "root",
-    password: "Txn@lgene11",
-    database: "content",
-  },
-  console.log(`Connected to the Content database.`));
+
+//Using Promises
+// class Database {
+//   constructor(config) {
+//     this.connection = mysql.createConnection(
+//       {
+//         host: process.env.DB_HOST,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PW,
+//         database: process.env.DB_NAME,
+//       },
+//       console.log(`Connected to the Content database.`)
+//     );
+//   }
+//   query(sql, args) {
+//     return new Promise((resolve, reject) => {
+//       this.connection.query( sql, args, (err, rows) => {
+//         if(err)
+//           return reject(err);
+//           resolve(rows);
+//       });
+//     });
+//   }
+//   close() {
+//     return new Promise((resolve, reject) => {
+//       this.connection.end( err => {
+//         if(err)
+//           return reject(err);
+//         resolve();
+//       });
+//     });
+//   }
+// }
+
+//Regular Connection
+// const db = mysql.createConnection(
+//   {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PW,
+//     database: process.env.DB_NAME,
+//   },
+//   console.log(`Connected to the Content database.`)
+// );
+
+const config = { host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PW, database: process.env.DB_NAME}
+
+function makeDb(config) {
+  const connection = mysql.createConnection(
+      {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME,
+      },
+      console.log(`Connected to the Content database.`)
+    );
+  return {
+    query(sql, args) {
+      return util.promisify(connection.query).call(connection, sql, args);
+    },
+    close() {
+      return util.promisify(connection.end).call(connection);
+    }
+  };
+}
+
+const db = makeDb(config);
 
 module.exports = db;
--- server.js
+++ server.js
@@ -6,25 +6,25 @@ let roles = [];
 let employees = [];
 let departments = [];
 
-function sqlQuery(query_str, params) {
-  db.query(query_str, params, (err, rows) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.table(rows);
-  });
-}
+// function sqlQuery(query_str, params) {
+//   try{ 
+//   const otherRows = await db.query(query_str, params, (err, rows));
+//   console.table(otherRows);
+//   } catch (err) {
+//     console.log(err);
+//     return;
+//   } finally {
+//     await db.close();
+//   };
+// }
 
+//How do I turn this into a promise statement?
 function pullDataQuery(query_str, params) {
   var tempPull = [];
-  db.query(query_str, params, (err, rows) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    for (var i = 0; i < rows.length; i++) {
-      tempPull.push(rows[i].name);
+  try {
+    const somerows = db.query(query_str, params);
+    for (var i = 0; i < somerows.length; i++) {
+      tempPull.push(somerows[i].name);
       console.log("Ln 29 Temp: ", tempPull);
     }
     if (query_str.includes("empRole")) {
@@ -36,7 +36,12 @@ function pullDataQuery(query_str, params) {
     } else {
       employees = tempPull;
     }
-  });
+  } catch (err) {
+    console.log(err);
+    return;
+  } finally {
+    db.close();
+  }
 }
 
 function fillData() {
@@ -49,10 +54,10 @@ function fillData() {
   departments.push(pullDataQuery(updateDep));
 
   var roleObjList = [];
-  pullDataQuery(updateRoles).then(({data}) => {
-    roleObjList.push(data)
-  });
-  // roleObjList.push(pullDataQuery(updateRoles));
+  // pullDataQuery(updateRoles).then(({data}) => {
+  //   roleObjList.push(data)
+  // });
+  roleObjList.push(pullDataQuery(updateRoles));
   console.log("Ln 53 roleObjList: ", roleObjList);
   //My roleObjList is returning undefined. How can I reset it?
   // for(var i=0; i<roleObjList.length; i++) {
@@ -93,88 +98,88 @@ function allChoices() {
     });
 }
 
-function nextUpPrompt(choice) {
-  let sql;
-  let params = [];
+// function nextUpPrompt(choice) {
+//   let sql;
+//   let params = [];
 
-  console.log("In Nextup Prompt: ", choice);
+//   console.log("In Nextup Prompt: ", choice);
 
-  switch (choice) {
-    case "View All Employees":
-      console.log("View All Emp: ", choice);
-      sql = `SELECT * FROM employee`;
-      sqlQuery(sql);
-      allChoices();
-      break;
-    case "View All Employees By Manager":
-      inquirer
-        .prompt([
-          {
-            type: "list",
-            name: "manager",
-            message: "Select a manager to view their employees:",
-            choices: employees,
-          },
-        ])
-        .then(({ manager }) => {
-          let managerId = employees.indexOf(manager) + 1;
-          (sql = `SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) AS employee, role.title AS role FROM employee
-        JOIN role ON employee.role_id = role.id
-        WHERE employee.manager_id = ?`),
-            (params = [managerId]),
-            sqlQuery(sql, params);
-        });
-      sql = `SELECT * FROM employee`;
-      sqlQuery(sql);
-    case "Add Employee":
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "first_name",
-            message: "What is the first name?",
-          },
-          {
-            type: "input",
-            name: "last_name",
-            message: "What is the last name?",
-          },
-          {
-            type: "list",
-            name: "role",
-            message: "What is the role?",
-            choices: roles,
-          },
-          {
-            type: "list",
-            name: "department",
-            message: "What department?",
-            choices: departments,
-          },
-          {
-            type: "list",
-            name: "manager",
-            //How can the manager be referenced by the manager id?
-            message: "Who is the manager?",
-            choices: employees,
-          },
-        ])
-        .then(({ first_name, last_name, role, department, manager }) => {
-          sql = `INSERT INTO employee (first_name, last_name, role, department, manager) VALUE (?,?,?)`;
-          params = [first_name, last_name, role, department, manager];
-          sqlQuery(sql, params);
-        })
-        .catch((err) => {
-          if (err) throw err;
-        });
-    case "Add Department":
-    case "Add Role":
-    case "View All Roles":
-    case "Remove Employee":
-    case "Update Employee Role":
-    case "Update Employee Manager":
-  }
-}
+//   switch (choice) {
+//     case "View All Employees":
+//       console.log("View All Emp: ", choice);
+//       sql = `SELECT * FROM employee`;
+//       sqlQuery(sql);
+//       allChoices();
+//       break;
+//     case "View All Employees By Manager":
+//       inquirer
+//         .prompt([
+//           {
+//             type: "list",
+//             name: "manager",
+//             message: "Select a manager to view their employees:",
+//             choices: employees,
+//           },
+//         ])
+//         .then(({ manager }) => {
+//           let managerId = employees.indexOf(manager) + 1;
+//           (sql = `SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) AS employee, role.title AS role FROM employee
+//         JOIN role ON employee.role_id = role.id
+//         WHERE employee.manager_id = ?`),
+//             (params = [managerId]),
+//             sqlQuery(sql, params);
+//         });
+//       sql = `SELECT * FROM employee`;
+//       sqlQuery(sql);
+//     case "Add Employee":
+//       inquirer
+//         .prompt([
+//           {
+//             type: "input",
+//             name: "first_name",
+//             message: "What is the first name?",
+//           },
+//           {
+//             type: "input",
+//             name: "last_name",
+//             message: "What is the last name?",
+//           },
+//           {
+//             type: "list",
+//             name: "role",
+//             message: "What is the role?",
+//             choices: roles,
+//           },
+//           {
+//             type: "list",
+//             name: "department",
+//             message: "What department?",
+//             choices: departments,
+//           },
+//           {
+//             type: "list",
+//             name: "manager",
+//             //How can the manager be referenced by the manager id?
+//             message: "Who is the manager?",
+//             choices: employees,
+//           },
+//         ])
+//         .then(({ first_name, last_name, role, department, manager }) => {
+//           sql = `INSERT INTO employee (first_name, last_name, role, department, manager) VALUE (?,?,?)`;
+//           params = [first_name, last_name, role, department, manager];
+//           sqlQuery(sql, params);
+//         })
+//         .catch((err) => {
+//           if (err) throw err;
+//         });
+//     case "Add Department":
+//     case "Add Role":
+//     case "View All Roles":
+//     case "Remove Employee":
+//     case "Update Employee Role":
+//     case "Update Employee Manager":
+//   }
+// }
 
 fillData();
 console.log(roles);
